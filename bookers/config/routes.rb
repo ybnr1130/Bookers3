e844efe45cec53aff4f89d81dc3a09971b9d673e
ybnr1130 <muuckk@gmail.com>
2021-08-25 19:13:45 +0000

    [Update]いいね機能※作成途中

--- bookers/config/routes.rb
+++ bookers/config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:show, :index, :edit, :update]
   resources :books, except: [:new] do
+    resources :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
   end
   # View作成から
