0ea3dbe045fc168eba68101b3cfb2b789f60441e
ybnr1130 <muuckk@gmail.com>
2021-08-26 09:45:36 +0000

    [Finish]

--- bookers/app/controllers/book_comments_controller.rb
+++ bookers/app/controllers/book_comments_controller.rb
@@ -1,11 +1,15 @@
 class BookCommentsController < ApplicationController
 
   def create
-    book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
-    comment.save
-    redirect_to book_path(book)
+    @book = Book.find(params[:book_id])
+    @comment = BookComment.new(book_comment_params)
+    @comment.book_id = @book.id
+    @comment.user_id = current_user.id
+    if @comment.save
+      redirect_to book_path(@book.id)
+    else
+      redirect_to book_path(@book)
+    end
   end
 
   def destroy
